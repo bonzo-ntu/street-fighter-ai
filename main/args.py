bd32a0103047fa4a184b9723b30c42a3ba251490
hi-i-m-gtooth <noface21105@gmail.com>
2023-05-28 02:50:37 +0800

    add time, score, time+score reward. add rd_type (eval_)reset_round args.

--- main/args.py
+++ main/args.py
@@ -10,6 +10,10 @@ def get_args():
     parser.add_argument('--n_steps', type=int, default=512, help='Number of steps')
     parser.add_argument('--total_timesteps', '-tt', type=int, default=10000000, help='Number of total timesteps')
     parser.add_argument('--check_timesteps', '-ct', type=int, default=500000, help='Number of timesteps to check')
+    parser.add_argument('--reset_round', '-rr', type=int, default=1, help='Number of rounds to reset. 0 means never reset')
+    parser.add_argument('--rd_type', '-rdt', type=str, default='default', help='Type of reward.')
+    # for evaluation
+    parser.add_argument('--eval_reset_round', '-evrr', type=int, default=1, help='Number of rounds to reset in evaluation. 0 means never reset')
     # for store results
     parser.add_argument('--log_dir', type=str, default='logs', help='Directory to save logs')
     parser.add_argument('--train_name', type=str, default='PPO', help='Name to be added to the path that saves models')
