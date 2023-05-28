de0cd0588018de24d24309317c4e4361ec92ff65
r11922012 <r11922012@ntu.edu.tw>
2023-05-28 21:09:24 +0800

    fix reward

--- main/rewarder.py
+++ main/rewarder.py
@@ -16,7 +16,6 @@ class CustomRewarder:
 
         # need to determine the rd_type
         self.essential_infokeys = self.get_essential_infokeys()
-        self.lose_rd, self.win_rd, self.fight_rd = self.get_rd_func()
 
     def get_essential_infokeys(self):
         default_keys = ["prev_oppont_health", "curr_oppont_health", "prev_player_health", "curr_player_health"]
@@ -32,36 +31,68 @@ class CustomRewarder:
         else:
             raise ValueError(f"rd_type should be {self.get_available_rd_types()}")
     
-    def get_rd_func(self):
+    def get_rd_lose(self):
         assert keys_are_in_dict(self.essential_infokeys, self.curr_info_dict)
         default_lose = -math.pow(self.full_hp, (self.curr_info_dict["curr_oppont_health"] + 1) / (self.full_hp + 1))
-        default_win = math.pow(self.full_hp, (self.curr_info_dict["curr_player_health"] + 1) / (self.full_hp + 1)) * self.rd_coeff
-        default_fight = self.rd_coeff * (self.curr_info_dict["prev_oppont_health"] - self.curr_info_dict["curr_oppont_health"]) - \
-                        (self.curr_info_dict["prev_player_health"] - self.curr_info_dict["curr_player_health"])
+
         if self.rd_type == "default": # 林亦原本的 Reward
             lose = default_lose
-            win = default_win
-            fight = default_fight
         elif self.rd_type == "time": # 自訂的 Reward 
             countdown_coeff = self.curr_info_dict["curr_countdown"] / self.curr_info_dict["init_countdown"]
             lose = default_lose * countdown_coeff
-            win = default_win * countdown_coeff
-            fight = default_fight * countdown_coeff
         elif self.rd_type == "score":
             score = self.curr_info_dict["curr_score"] - self.curr_info_dict["prev_score"]
             lose = score
-            win = score
-            fight = score
         elif self.rd_type == "time+score":
             score = self.curr_info_dict["curr_score"] - self.curr_info_dict["prev_score"]
             countdown_coeff = self.curr_info_dict["curr_countdown"] / self.curr_info_dict["init_countdown"]
             lose = score * countdown_coeff
+        else:
+            raise ValueError(f"rd_type should be {self.get_available_rd_types()}")
+        
+        return lose
+
+    def get_rd_win(self):
+        assert keys_are_in_dict(self.essential_infokeys, self.curr_info_dict)
+        default_win = math.pow(self.full_hp, (self.curr_info_dict["curr_player_health"] + 1) / (self.full_hp + 1)) * self.rd_coeff
+        
+        if self.rd_type == "default": # 林亦原本的 Reward
+            win = default_win
+        elif self.rd_type == "time": # 自訂的 Reward 
+            countdown_coeff = self.curr_info_dict["curr_countdown"] / self.curr_info_dict["init_countdown"]
+            win = default_win * countdown_coeff
+        elif self.rd_type == "score":
+            score = self.curr_info_dict["curr_score"] - self.curr_info_dict["prev_score"]
+            win = score
+        elif self.rd_type == "time+score":
+            score = self.curr_info_dict["curr_score"] - self.curr_info_dict["prev_score"]
+            countdown_coeff = self.curr_info_dict["curr_countdown"] / self.curr_info_dict["init_countdown"]
             win = score * countdown_coeff
+        else:
+            raise ValueError(f"rd_type should be {self.get_available_rd_types()}")
+        
+        return win
+    
+    def get_rd_fight(self):
+        assert keys_are_in_dict(self.essential_infokeys, self.curr_info_dict)
+        default_fight = self.rd_coeff * (self.curr_info_dict["prev_oppont_health"] - self.curr_info_dict["curr_oppont_health"]) - \
+                        (self.curr_info_dict["prev_player_health"] - self.curr_info_dict["curr_player_health"])
+        if self.rd_type == "default": # 林亦原本的 Reward
+            fight = default_fight
+        elif self.rd_type == "time": # 自訂的 Reward 
+            countdown_coeff = self.curr_info_dict["curr_countdown"] / self.curr_info_dict["init_countdown"]
+            fight = default_fight * countdown_coeff
+        elif self.rd_type == "score":
+            score = self.curr_info_dict["curr_score"] - self.curr_info_dict["prev_score"]
+            fight = score
+        elif self.rd_type == "time+score":
+            score = self.curr_info_dict["curr_score"] - self.curr_info_dict["prev_score"]
+            countdown_coeff = self.curr_info_dict["curr_countdown"] / self.curr_info_dict["init_countdown"]
             fight = score * countdown_coeff
         else:
             raise ValueError(f"rd_type should be {self.get_available_rd_types()}")
         
-        return lose, win, fight
+        return fight
     
     def norm(self, rd):
         # 因為已經跑實驗了，所以就把 Normalized 的 Reward 上界定成 0.5
@@ -87,11 +118,11 @@ class CustomRewarder:
     
     ## Get normalized rewards
     def lose(self):
-        return self.norm(self.lose_rd)
+        return self.norm(self.get_rd_lose())
     def win(self):
-        return self.norm(self.win_rd)
+        return self.norm(self.get_rd_win())
     def fight(self):
-        return self.norm(self.fight_rd)
+        return self.norm(self.get_rd_fight())
     
     ## Tools
     @staticmethod
